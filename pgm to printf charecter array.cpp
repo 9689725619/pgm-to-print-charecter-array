340996115e493a2f3cb98ba5847275a1be284aa0
Shubhangi meshram <101330420+9689725619@users.noreply.github.com>
2022-03-29 17:33:37 +0530

    Add files via upload

---
+++ pgm to printf charecter array.cpp
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+	
+	char name[]={'s','y','s','t','e','m'};
+	int j;
+	printf("the given charecter are :%s",name);
+	for(j=0;j<6;++j)
+	{
+		printf("\n%c",name[j]);
+	}
+	   
+   
+	return 0;
+	
+}
